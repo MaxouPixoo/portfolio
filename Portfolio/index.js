cb8c0ba3554b6716670e962c44565b86d273e7af
Maxence Vechambre <Maxence.VECHAMBRE@etu.uca.fr>
2026-06-21 22:28:10 +0200

    70%

---
+++ Portfolio/index.js
@@ -0,0 +1,43 @@
+﻿const express = require('express')
+const app = express()
+const port = 3000
+
+const hbs = require("hbs");
+const path = require("path");
+app.use(express.static("public"));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/projet/MatinBrun', (req, res) => {
+    res.render('projects/MatinBrun');
+})
+
+app.get('/projet/VReeducation', (req, res) => {
+    res.render('projects/VReeducation');
+})
+
+app.get('/projet/HeroForge', (req, res) => {
+    res.render('projects/HeroForge');
+})
+
+app.get('/projet/Vapeur', (req, res) => {
+    res.render('projects/Vapeur');
+})
+
+app.get('/projet/RouagesDuDestin', (req, res) => {
+    res.render('projects/RouagesDuDestin');
+})
+
+app.get('/projet/SpiritGame', (req, res) => {
+    res.render('projects/SpiritGame');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ Portfolio/public/script/header.js
@@ -0,0 +1,35 @@
+﻿const header = document.getElementById("nomPseudoHeader");
+
+function changeText(newText) {
+    header.style.opacity = "0"; // fade out
+
+    setTimeout(() => {
+        header.textContent = newText; // changer le texte
+        header.style.opacity = "1";   // fade in
+    }, 300);
+}
+
+document.addEventListener("mouseover", (event) => {
+    if (event.target.id === "nomPseudoHeader") {
+        changeText("Maxou Pixou");
+    }
+});
+
+document.addEventListener("mouseout", (event) => {
+    if (event.target.id === "nomPseudoHeader") {
+        changeText("Maxence Véchambre");
+    }
+});
+
+window.addEventListener("popstate", async () => {
+
+    const response = await fetch(location.pathname);
+    const html = await response.text();
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+
+    document.querySelector("#content").innerHTML =
+        doc.querySelector("#content").innerHTML;
+
+});
